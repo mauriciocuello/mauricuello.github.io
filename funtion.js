71be7fb5a5c122a179c8237c6f99360e4e8b6734
mauricuello <126887473+mauriciocuello@users.noreply.github.com>
2023-03-20 22:11:20 -0300

    Add files via upload

---
+++ funtion.js
@@ -0,0 +1,6 @@
+document.getElementById("boton").onclick = function name() {
+    document.getElementById("mail").innerHTML = "mauricio_cuello16@hotmail.com";
+}
+document.getElementById("lkd").addEventListener ("click", e => {
+    window.location.href = "https://www.linkedin.com/in/mauricio-cuello-001a768a/"
+})
